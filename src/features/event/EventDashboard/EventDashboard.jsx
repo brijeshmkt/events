08ecc0f6aa8fa9b65a3e2a2dafb4d898d128b7da
Brijesh Mishra <brijeshmishra@Brijeshs-MacBook-Pro.local>
2019-10-25 17:01:52 +0530

    UI part

---
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
+import EventList from '../EventList/EventList'
+import EventForm from '../EventForm/EventForm'
+
+class EventDashboard extends Component {
+    render() {
+        return (
+            <Grid>
+                <Grid.Column width={10}>
+                    <EventList />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <EventForm />
+                </Grid.Column>
+            </Grid>
+        )
+    }
+}
+
+export default EventDashboard;
---
+++ src/features/event/EventForm/EventForm.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { Segment, Grid, Header, Form, Field, Button } from 'semantic-ui-react'
+
+export default class EventForm extends Component {
+    render() {
+        return (
+            
+        <Segment>
+            <Form>
+                <Form.Field>
+                    <label>Event Title</label>
+                    <input placeholder="First name"/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Event Date</label>
+                    <input type="date" placeholder="First name"/>
+                </Form.Field>
+                <Form.Field>
+                    <label>City</label>
+                    <input type="date" placeholder="First name"/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Venue</label>
+                    <input type="date" placeholder="First name"/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Hosted By</label>
+                    <input type="date" placeholder="First name"/>
+                </Form.Field>
+                <Button positive type="submit">Submit</Button>
+                <Button type="button">Cancel</Button>
+            </Form>
+        </Segment>
+       
+        );
+    }
+}
